267e20ea63c6a96f8e7f5d70161d0b58a6e4b9a7
Adam Johnston <adamjohnston151@yahoo.com>
2019-03-17 18:20:16 -0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Diagnostics;
+
+namespace UsbDetection
+{
+    class Program
+    {
+        static void Main()
+        {
+            WqlEventQuery insertQuery = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+            ManagementEventWatcher insertWatcher = new ManagementEventWatcher(insertQuery);
+            insertWatcher.EventArrived += new EventArrivedEventHandler(DeviceInsertedEvent);
+            insertWatcher.Start();
+
+            WqlEventQuery removeQuery = new WqlEventQuery("SELECT * FROM __InstanceDeletionEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+            ManagementEventWatcher removeWatcher = new ManagementEventWatcher(removeQuery);
+            removeWatcher.EventArrived += new EventArrivedEventHandler(DeviceRemovedEvent);
+            removeWatcher.Start();
+
+            void DeviceInsertedEvent(object sender, EventArrivedEventArgs e)
+            {
+                ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+                foreach (var property in instance.Properties)
+                {
+                    Console.WriteLine(property.Name + " = " + property.Value);
+                }
+            }
+
+            void DeviceRemovedEvent(object sender, EventArrivedEventArgs e)
+            {
+                ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+                foreach (var property in instance.Properties)
+                {
+                    Console.WriteLine(property.Name + " = " + property.Value);
+                }
+            }
+            Process.GetCurrentProcess().WaitForExit();
+        }
+    }
+}
